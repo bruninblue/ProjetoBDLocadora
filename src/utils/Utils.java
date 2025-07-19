8534b87df6fa0d2860e11bf90998851433a82f55
LeviOlii <levi.bernardo0909@gmail.com>
2025-07-19 19:57:25 -0300

    add utils: funcao somarDias | começando menuAluguel

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,28 +3,37 @@ package utils;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.sql.Date;
+
 import conexao.Conexao;
 
 public class Utils {
-    public static int verificarExistenciaCliente(String cpf){
-        int verificador =  0;
+    public static int verificarExistenciaCliente(String cpf) {
+        int verificador = 0;
         String query_select = "SELECT * FROM CLIENTE WHERE CPF=?";
         PreparedStatement ps = null;
 
-        try{
+        try {
             ps = Conexao.getConexao().prepareStatement(query_select);
             ps.setString(1, cpf);
             ResultSet resultado = ps.executeQuery();
-            
+
             while (resultado.next()) {
                 verificador++;
             }
 
             ps.close();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Erro ao consultar: " + e.getMessage());
         }
-        
+
         return verificador;
+    }
+
+    public static Date somarDias(int dias) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, dias);
+        return new Date(calendar.getTimeInMillis());
     }
 }
